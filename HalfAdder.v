f001d068b1c2010568e068446fb3e101b88cafc5
Smith <codingfreak98@69FuckOfflilpp.local>
2020-06-11 17:16:12 +0530

    add main ALU code

--- HalfAdder.v
+++ HalfAdder.v
@@ -2,12 +2,15 @@ module HalfAdder(a,b,sum,carry);
     // Inputs
     input[31:0]a;   // 32 bit input a
     input[31:0]b;   // 32 bit input b
+
     // Outputs
     output[31:0]sum;  // sum of a+b
     output[31:0]carry;  // carry of a+b
+
     // declare the regs
     reg[31:0]sum;
     reg[31:0]carry;
+    
     // main logic
     always@(a, b)
     begin
